80f7516a7c4ae2eb1c7f7817f16bf35348391450
mplessis <mplessis@hotmail.com>
2016-09-06 17:34:48 +0200

    Nouvel assembly spécial UWP et ajout d'un helper UI avec la fonction GetTypeDeviceRun

--- UWP/Kopigi.NetCore.UWP/Properties/AssemblyInfo.cs
+++ UWP/Kopigi.NetCore.UWP/Properties/AssemblyInfo.cs
@@ -1,20 +1,18 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kopigi.Portable")]
+[assembly: AssemblyTitle("Kopigi.NetCore.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kopigi.Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Kopigi.NetCore.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.1.0")]
 [assembly: AssemblyFileVersion("1.8.1.0")]
+[assembly: ComVisible(false)]
---
+++ UWP/Kopigi.NetCore.UWP/UI/UIHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+
+namespace Kopigi.NetCore.UWP.UI
+{
+    public static class UIHelper
+    {
+        /// <summary>
+        /// Permet de savoir sur quel type de device l'application tourne
+        /// </summary>
+        /// <returns></returns>
+        public static TypeDeviceRun GetTypeDeviceRun()
+        {
+            switch (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                case "Windows.Mobile":
+                    return TypeDeviceRun.Mobile;
+                case "Windows.Desktop":
+                    return UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse ? TypeDeviceRun.Desktop : TypeDeviceRun.Tablet;
+                case "Windows.Universal":
+                    return TypeDeviceRun.IoT;
+                case "Windows.Team":
+                    return TypeDeviceRun.SurfaceHub;
+                case "Windows.Xbox":
+                    return TypeDeviceRun.Xbox;
+                default:
+                    return TypeDeviceRun.Other;
+            }
+        }
+    }
+
+    public enum TypeDeviceRun
+    {
+        Desktop,
+        Tablet,
+        Mobile,
+        IoT,
+        SurfaceHub,
+        Xbox,
+        Other
+    }
+}
