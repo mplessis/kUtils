edf408cc8276716436d79ee0dca7155c5ef5dc53
mplessis <mplessis@hotmail.com>
2016-10-11 12:11:28 +0200

    Ajout d'un namespace Extensions sur Kopigi.Portable, premiére fonction ToSeparateByChar(this IEnumerable<T> list, char separate) pour la classe StringExtensions

---
+++ Portable/Kopigi.Portable/Extensions/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kopigi.Portable.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Permet d'exporter une liste de T, vers une string dont chaque valeur est séparée par un char défini
+        /// </summary>
+        /// <param name="list">Liste d'objets à séparer</param>
+        /// <param name="separate">Caractére de séparation</param>
+        /// <returns></returns>
+        public static string ToSeparateByChar<T>(this IEnumerable<T> list, char separate)
+        {
+            if (list.Any())
+            {
+                var itemsSeparateBuilder = new StringBuilder();
+                foreach (var item in list)
+                {
+                    itemsSeparateBuilder.Append($"{item.ToString()}{separate}");
+                }
+                return itemsSeparateBuilder.Remove(itemsSeparateBuilder.Length - 1, 1).ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
