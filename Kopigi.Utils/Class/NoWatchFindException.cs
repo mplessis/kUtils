9b6da9705ffe001a0905a611ceb02a3a2531d09d
Marc PLESSIS <marc.plessis@legrand.fr>
2018-11-23 11:21:09 +0100

    Add ISerializable implementation for NoWatchFindException

--- Kopigi.Utils/Class/NoWatchFindException.cs
+++ Kopigi.Utils/Class/NoWatchFindException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Kopigi.Utils.Class
 {
+    [Serializable]
     public class NoWatchFindException : Exception
     {
         /// <summary>
@@ -10,6 +12,9 @@ namespace Kopigi.Utils.Class
         /// <param name="label">Label de surveillance demandé</param>
         public NoWatchFindException(string label)
             : base($"No watch found for label {label}", null)
+        { }
+
+        protected NoWatchFindException(SerializationInfo info, StreamingContext context) : base(info, context)
         { }
     }
 }
