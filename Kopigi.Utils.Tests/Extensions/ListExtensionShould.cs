24f305192dff7ab5e835ba16a3eb1c64e3551859
Marc Plessis <mplessis@hotmail.com>
2018-11-23 22:28:41 +0100

    Add List<T> extension for return string with separator char

---
+++ Kopigi.Utils.Tests/Extensions/ListExtensionShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kopigi.Utils.Extensions;
+using NFluent;
+using NUnit.Framework;
+
+namespace Kopigi.Utils.Tests.Extensions
+{
+    public class ListExtensionShould
+    {
+        [Test]
+        public void Return_1_2_3_for_list_which_contains_1_2_3()
+        {
+            Check.That(new List<int>() {1, 2, 3}.ToSeparateByChar('$')).IsEqualTo("1$2$3");
+        }
+
+        [Test]
+        public void Return_string_empty_if_list_is_empty()
+        {
+            Check.That(new List<int>().ToSeparateByChar(' ')).IsEqualTo("");
+        }
+    }
+}
---
+++ Kopigi.Utils/Extensions/ListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kopigi.Utils.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Permet d'exporter une liste de T, vers une string dont chaque valeur est séparée par un char défini
+        /// </summary>
+        /// <param name="list">Liste d'objets à séparer</param>
+        /// <param name="separate">Caractére de séparation</param>
+        /// <returns></returns>
+        public static string ToSeparateByChar<T>(this IEnumerable<T> list, char separate)
+        {
+            if (list.Any())
+            {
+                var itemsSeparateBuilder = new StringBuilder();
+                foreach (var item in list)
+                {
+                    itemsSeparateBuilder.Append($"{item.ToString()}{separate}");
+                }
+                return itemsSeparateBuilder.Remove(itemsSeparateBuilder.Length - 1, 1).ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
